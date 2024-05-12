d3269cd85bf84e8bd4182ab7bf0822c0a6a4ec7a
destinyfkrr <destinyfkrr@users.noreply.github.com>
2024-05-12 18:21:17 +0530

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,21 +1,24 @@
 public class InsertionSort {
     public static void main(String[] args) {
         int[] array = {31, 42, 13, 63, 52, 69};
-        int x = 25;
-        int position = 0;
-        for (int i = 0; i < 6; i++) {
-            if (array[i] > x) {
-                position = i;
-                break;
-            }
+        insertionSort(arr);
+        System.out.println("Sorted array in ascending order:");
+        for (int num : arr) {
+            System.out.print(num + " ");
         }
-        for (int i = 6; i > position; i--) {
-            array[i] = array[i - 1];
-        }
-        array[position] = x;
-        System.out.println("Example :");
-        printArray(array);
-
     }
 
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
 }
